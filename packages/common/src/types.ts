dc970fe0aede879b29c3ef6da4f735b59f6779bd
ghagevaibhav <ghagevaibhav@gmail.com>
2025-01-16 21:58:08 +0530

    packages added

--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -2,15 +2,15 @@ import z from 'zod'
 
 export const CreateUserSchema = z.object({
     username: z.string().min(5).max(15),
-    password: z.string().min(8).max(16),
-    name: z.string().min(5)
+    password: z.string().min(3),
+    name: z.string()
 })
 
 export const SignInSchema = z.object({
     username: z.string().min(5).max(15),
-    password: z.string().min(8).max(16),
+    password: z.string().min(3),
 })
 
 export const CreateRoomSchema = z.object({
-    name: z.string().min(3).max(15)
+    name: z.string().min(3).max(20)
 })
